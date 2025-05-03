8f07504fcbe26b39e1116e8abd6f57141c5a93a2
George Lee <george10lee@gmail.com>
2025-05-03 07:14:39 -0700

    completePart1

---
+++ iQuiz/quizCell.swift
@@ -0,0 +1,21 @@
+//
+//  quizCell.swift
+//  iQuiz
+//
+//  Created by George Lee on 5/3/25.
+//
+
+import UIKit
+
+class quizCell: UITableViewCell {
+
+
+    
+    @IBOutlet weak var quizImage: UIImageView!
+    
+    
+    @IBOutlet weak var quizTitle: UILabel!
+    
+    @IBOutlet weak var quizDesc: UILabel!
+
+}
